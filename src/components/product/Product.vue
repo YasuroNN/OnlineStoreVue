419b328d0cc1630ec0ece57ef998c07e1111e055
yasuro <voronov.maksim.v@gmail.com>
2020-11-27 23:18:21 +0300

    add product and listProduct UI

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -1,12 +1,25 @@
 <template>
   <!-- Базовая разметка для кмопонет согласно сетке -->
-    <v-container>
-      <v-layout row>
-        <v-flex xs12>
-          <h1>Product</h1>
+  <v-container>
+    <section class="product mt3 elevation-10">
+      <v-layout row wrap>
+        <v-flex xs12 lg6>
+          <img src="https://items.s1.citilink.ru/1376001_v01_b.jpg" class="product_img" />
+        </v-flex>
+        <v-flex xs12 lg6>
+          <div class="product_info">
+            <h5 class="product_title display-1 mb-3 mt-3">lorem lorem</h5>
+            <p class="product_category_titile">
+              <span class="product_titile">Venodor: </span>Lorem
+            </p>
+            <p class="product_price title">
+              <span class="product_title">Price: </span>$1000
+            </p>
+          </div>
         </v-flex>
       </v-layout>
-    </v-container>
+    </section>
+  </v-container>
 </template>
 
 <script>
@@ -16,3 +29,31 @@ export default {
   }
 };
 </script>
+<style scoped>
+.product {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  border: 1px solid #1c77ad;
+  padding: 30px;
+  margin-bottom: 100px;
+}
+
+.product_img {
+  height: 200px;
+  width: 550px;
+}
+
+.product_info {
+  margin-left: 50px;
+}
+.product_titile {
+  font-size: 1.6em;
+  font-weight: bold;
+  color: blue;
+  margin-bottom: 0;
+}
+.product_price {
+  color: red;
+}
+</style>
--- src/components/product/ProductList.vue
+++ src/components/product/ProductList.vue
@@ -2,8 +2,25 @@
   <!-- Базовая разметка для кмопонет согласно сетке -->
     <v-container>
       <v-layout row>
-        <v-flex xs12>
-          <h1>ProductList</h1>
+        <v-flex xs12 sm6 offset-sm3>
+          <h1 class="text--secondary mb3">ProductList</h1>
+          <v-card class="elevation-10 mb-2" v-for="(product, i) in products" :key="i">
+            <v-layout>
+              <v-flex xs4>
+                <v-img :src="product.imageSrc"></v-img>
+              </v-flex>
+              <v-flex xs8>
+                <v-card-text>
+                  <h2 class="text--primary"> {{product.title }}</h2>
+                  <p>{{product.venodr }}</p>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer />
+                  <v-btn class="info" :to="'/product/' + product.id">Open</v-btn>
+                </v-card-actions>
+              </v-flex>
+            </v-layout>
+          </v-card>
         </v-flex>
       </v-layout>
     </v-container>
@@ -12,7 +29,32 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+       products: [
+        {
+          id: "1",
+          title: "Lenovo Legion",
+          venodr: "Lenovo",
+          price: 100,
+          imageSrc: "https://cdn1.ozone.ru/s3/multimedia-m/6007603366.jpg"
+        },
+        {
+          id: "2",
+          title: "MacBook",
+          venodr: "apple",
+          price: 200,
+          imageSrc: "https://items.s1.citilink.ru/1376001_v01_b.jpg"
+        },
+        {
+          id: "3",
+          title: "Toshiba ls200",
+          venodr: "Toshiba",
+          price: 400,
+          imageSrc:
+            "https://www.notebookcheck-ru.com/uploads/tx_nbc2/ToshibaSatelliteProR40-C__1_.JPG"
+        }
+      ]
+    };
   }
 };
 </script>
