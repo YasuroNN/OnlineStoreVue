79c508d2188024721bb39f49548bd6be866ec274
yasuro <voronov.maksim.v@gmail.com>
2020-11-27 13:18:49 +0300

    add base layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
+<!-- 
+
+<v-app></v-app> - аналог div id = app из vue 
+<v-main>- главный html элемент. В нем все происходит. он занимате весь экран.
+<v-app-bar app> это типо тулбар. Директива app говорит vuetify что это элемент разметки, т.е эта директива влият на <v-main>. 
+  Когда есть эта директива, то <v-main> пересчитывается исходя из того, сколько пространства занимает элемент (<v-app-bar>). 
+  В данном случае v-app-bar втсанет в самом верху, а контент </v-main> будет распологаться под ним.
+
+-->
 <template>
-<div>
-  Hellow vue
-</div>
+  <v-app>
+    <v-app-bar app></v-app-bar>
+    <v-main>
+      <v-container>Hello World</v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
